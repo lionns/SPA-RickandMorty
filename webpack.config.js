078bc234263040b829f6da641b55678a455f6ffb
lionns <juans.leonv@icloud.com>
2022-01-09 21:42:16 -0500

    Consuming API data and applying CSS Styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    clean: true,
   },
   resolve: {
     extensions: [".js"],
@@ -26,6 +28,14 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'styles/styles.css'),
+          to: '',
+        }
+      ]
     }),
   ],
 };
